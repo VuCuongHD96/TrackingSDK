38c5ca8ed22273cd398ba48014c224267cafdef0
Sun <sun@Suns-MacBook-Pro.local>
2021-10-03 20:48:25 +0700

    Base SDK

---
+++ TrackingSDK/TrackingSDK.h
@@ -0,0 +1,19 @@
+//
+//  TrackingSDK.h
+//  TrackingSDK
+//
+//  Created by Sun on 10/3/21.
+//  Copyright © 2021 Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TrackingSDK.
+FOUNDATION_EXPORT double TrackingSDKVersionNumber;
+
+//! Project version string for TrackingSDK.
+FOUNDATION_EXPORT const unsigned char TrackingSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrackingSDK/PublicHeader.h>
+
+
